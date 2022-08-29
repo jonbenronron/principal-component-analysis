b81d0529315e3a77fa6a45ddfd1e98b54b169c45
jonbenronron <ronijonathanbk@gmail.com>
2022-08-29 23:09:38 +0300

    fix: removed unnecessary clones

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,24 @@ use num_traits::cast::AsPrimitive;
 fn main() {
     let x: Vec<f32> = vec![1., 2., 3.];
     let y: Vec<f32> = vec![2., 3., 4.];
-    println!("Mean of X: {:?}", mean::<f32, f32>(x.clone()));
-    println!("Mean of Y: {:?}", mean::<f32, f32>(y.clone()));
-    println!("Covariance of X and Y: {:?}", covariance(x.clone(), y.clone()))
+    println!("Mean of X: {:?}", mean::<f32, f32>(&x));
+    println!("Mean of Y: {:?}", mean::<f32, f32>(&y));
+    println!("Covariance of X and Y: {:?}", covariance(&x, &y))
 }
 
-fn covariance(x: Vec<f32>, y: Vec<f32>) -> f32 {
+fn covariance(x: &Vec<f32>, y: &Vec<f32>) -> f32 {
     assert!(!x.is_empty(), "Vector X can't be empty!");
     assert!(!y.is_empty(), "Vector Y can't be empty!");
     assert_eq!(x.len(), y.len(), "Vectors of covariance has to be same length!");
 
-    let xy_mean: f32 = mean(dot(x.clone(), y.clone()));
-    let x_mean: f32 = mean(x.clone());
-    let y_mean: f32 = mean(y.clone());
+    let xy_mean: f32 = mean(&dot(x, y));
+    let x_mean: f32 = mean(x);
+    let y_mean: f32 = mean(y);
 
     xy_mean - x_mean * y_mean
 }
 
-fn mean<T, R>(x: Vec<T>) -> R
+fn mean<T, R>(x: &Vec<T>) -> R
     where R: Div<R, Output=R> + Sum<R> + Copy + 'static,
           T: AsPrimitive<R>,
           usize: AsPrimitive<R>
@@ -36,7 +36,7 @@ fn mean<T, R>(x: Vec<T>) -> R
     
 }
 
-fn dot(x: Vec<f32>, y: Vec<f32>) -> Vec<f32> {
+fn dot(x: &Vec<f32>, y: &Vec<f32>) -> Vec<f32> {
     assert!(!x.is_empty(), "Vector X can't be empty!");
     assert!(!y.is_empty(), "Vector Y can't be empty!");
     assert_eq!(x.len(), y.len(), "Vectors of dot product has to be same length!");
